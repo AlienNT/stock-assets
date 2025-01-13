2d989f574d50c0c6523a1a9ae4fceaba82f2ed59
Trysten M <nick.tristen22@gmail.com>
2025-01-14 00:28:22 +0200

    add(types) add video types

---
+++ src/types/VideoTypes.ts
@@ -0,0 +1,26 @@
+import {BaseHitInterface, BaseRequestInterface, BaseResponseInterface, VideoType} from "./BaseSearchTypes.ts";
+
+export interface VideoResponseFieldsInterface {
+    url: string,
+    width: number,
+    height: number,
+    size: number,
+    thumbnail: string
+}
+
+export interface VideoRequestInterface extends BaseRequestInterface {
+    video_type: VideoType
+}
+
+export interface VideoHitInterface extends BaseHitInterface {
+    type: VideoType,
+    tags: string,
+    duration: number,
+    videos: {
+        large?: VideoResponseFieldsInterface,
+        medium?: VideoResponseFieldsInterface,
+        small?: VideoResponseFieldsInterface,
+        tiny?: VideoResponseFieldsInterface,
+    }
+}
+export interface VideoResponseInterface extends BaseResponseInterface<VideoHitInterface> {}
