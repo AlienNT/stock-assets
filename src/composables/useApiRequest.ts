50736c8a95b32761c6cfc72da15c5c9a3aedecca
Trysten M <nick.tristen22@gmail.com>
2025-01-14 00:31:10 +0200

    add(composable) add api request

---
+++ src/composables/useApiRequest.ts
@@ -0,0 +1,53 @@
+import {computed, reactive} from "vue";
+import {apiConfig} from "../configs/apiConfig.ts";
+import axios, {AxiosProgressEvent, AxiosResponse} from "axios";
+
+export interface ApiRequestInterface<RequestInterface> {
+    method?: 'GET',
+    params?: RequestInterface,
+    url?: string,
+}
+
+export function useApiRequest() {
+    const state = reactive({
+        isLoading: false,
+        onDownloadProgress: {} as AxiosProgressEvent,
+        onUploadProgress: {} as AxiosProgressEvent,
+    })
+
+    const isLoading = computed(() => state.isLoading);
+    const downloadProgress = computed(() => state.onDownloadProgress);
+    const uploadProgress = computed(() => state.onUploadProgress);
+
+    async function request<RequestInterface, ResponseInterface>(
+        {
+            method = 'GET',
+            params,
+            url,
+        }: ApiRequestInterface<RequestInterface>): Promise<AxiosResponse<ResponseInterface>> {
+
+        state.isLoading = true;
+
+        return axios({
+            baseURL: apiConfig.API_URL,
+            method,
+            url,
+            params: {...params, key: apiConfig.API_KEY},
+            onDownloadProgress(progressEvent: AxiosProgressEvent) {
+                state.onDownloadProgress = progressEvent
+            },
+            onUploadProgress(progressEvent: AxiosProgressEvent) {
+                state.onUploadProgress = progressEvent
+            },
+        }).finally(() => {
+            state.isLoading = false
+        })
+    }
+
+    return {
+        request,
+        isLoading,
+        downloadProgress,
+        uploadProgress,
+    }
+}
