0fd4c65a2ac2739a8cb7c0db458ec08723bb6834
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:19:31 +0200

    add (components) create video list component

---
+++ src/components/video/VideoList.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import {VideoHitInterface} from "@/types/VideoTypes.ts";
+import VideoListItem from "@/components/video/VideoListItem.vue";
+import MasonryTemplate from "@/templates/MasonryTemplate.vue";
+
+import {calcHeight} from "@/helpers/templateHelper.ts";
+import {useConfig} from "@/composables/useConfig.ts";
+
+export interface VideoListPropsInterface {
+  videos: VideoHitInterface[]
+}
+
+defineProps<VideoListPropsInterface>()
+
+const emit = defineEmits(['onScrolled'])
+
+const {appConfig} = useConfig()
+
+function calcItemHeight(item: VideoHitInterface, itemWidth: number): number {
+  return calcHeight({
+    width: item.videos?.tiny?.width,
+    height: item.videos?.tiny?.height,
+    itemWidth
+  })
+}
+
+</script>
+
+<template>
+  <MasonryTemplate
+      :items="videos"
+      :max-column-count="appConfig.VIDEO_LIST_COLUMNS_MAX"
+      :min-column-count="appConfig.VIDEO_LIST_COLUMNS_MIN"
+      :gap="appConfig.VIDEO_LIST_GAP"
+      :calc-item-height="calcItemHeight"
+      @on-scrolled="emit('onScrolled')"
+  >
+    <template #content="{item}: {item: VideoHitInterface}">
+        <VideoListItem
+            class="video-list-item"
+            :videos="item.videos"
+            :comments="item.comments"
+            :downloads="item.downloads"
+            :duration="item.duration"
+            :value="item.id"
+            :likes="item.likes"
+            :user="item.user"
+            :user_id="item.user_id"
+            :tags="item.tags"
+            :type="item.type"
+            :views="item.views"
+            :id="item.id"
+            :page-u-r-l="item.pageURL"
+            :user-image-u-r-l="item.userImageURL"/>
+    </template>
+  </MasonryTemplate>
+</template>
+
+<style scoped lang="scss">
+</style>
