73122855c587633cb44ead9f7c3d6d994f8b19ce
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:14:46 +0200

    update (router) add routes for image & video detailed pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,10 +7,20 @@ const routes = [
         component: () => import('@/pages/ImagesPage.vue')
     },
     {
+        path: '/Images/:id',
+        name: 'Images',
+        component: () => import('@/pages/ImageDetailPage.vue')
+    },
+    {
         path: '/videos',
         name: 'Videos',
-        component: () => import('@/pages/VideosPage.vue')
-    }
+        component: () => import('@/pages/VideosPage.vue'),
+    },
+    {
+        path: '/videos/:id',
+        name: 'Video',
+        component: () => import('@/pages/VideoDetailPage.vue')
+    },
 ] as RouteRecordRaw[]
 
 export default routes
