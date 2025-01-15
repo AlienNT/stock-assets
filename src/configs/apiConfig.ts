c096c4f1785b478ffb0ac71416c80ba96cbdea56
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:13:43 +0200

    add (configs) add api config types

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -1,4 +1,10 @@
-export const apiConfig = {
+export interface ApiConfigInterface {
+    API_URL: string;
+    API_KEY: string;
+}
+
+export const apiConfig: ApiConfigInterface = {
     API_URL: 'https://pixabay.com/api/',
     API_KEY: import.meta.env.VITE_API_KEY || '',
 }
+export default apiConfig;
