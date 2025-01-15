22ab9376f5200d7b98f8464d4468f3d87224cdb0
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:21:28 +0200

    add (templates) create detailed page template

---
+++ src/templates/DetailedPageTemplate.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="detailed-page-template">
+    <div class="detailed-page-content">
+      <slot name="content"/>
+    </div>
+    <div class="detailed-page-info">
+      <slot name="info"/>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.detailed-page-template {
+  display: flex;
+  flex-wrap: wrap;
+}
+.detailed-page-content {
+  flex: 1 1 65%;
+  display: flex;
+  flex-direction: column;
+}
+.detailed-page-info {
+  flex: 1 1 35%;
+}
+</style>
