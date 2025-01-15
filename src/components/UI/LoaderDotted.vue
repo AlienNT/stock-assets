ed80cec1510af5d1061c40ddad586cce79b65a78
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:18:15 +0200

    add (components) create dotted loader component

---
+++ src/components/UI/LoaderDotted.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="loader-container">
+   <div class="loader-wrapper">
+     <span class="loader"></span>
+   </div>
+ </div>
+</template>
+
+<style scoped lang="scss">
+.loader-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+  overflow: hidden;
+}
+.loader-wrapper {
+  height: 50px;
+  width: 50px;
+
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+
+}
+
+.loader {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  padding-left: 100%;
+  padding-top: 100%;
+  border-radius: 50%;
+  display: inline-block;
+  border-top: 3px solid #585a9a;
+  border-right: 3px solid transparent;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
