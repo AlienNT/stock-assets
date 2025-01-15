dd0f1958dcefdf2395248da09aae9c20346daf04
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:19:50 +0200

    add (components) create detailed image component

---
+++ src/components/image/DetailedImage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {ImageHitInterface} from "@/types/ImageTypes.ts";
+import VImage from "@/components/UI/VImage.vue";
+
+export interface DetailedImageProps extends ImageHitInterface {
+}
+
+defineProps<DetailedImageProps>()
+</script>
+
+<template>
+<div class="detailed-image">
+  <VImage
+      :src="webformatURL"
+      :background="'transparent'"
+      alt="image"
+  />
+</div>
+</template>
+
+<style scoped lang="scss">
+.detailed-image {
+  flex: 1;
+  display: flex;
+}
+</style>
