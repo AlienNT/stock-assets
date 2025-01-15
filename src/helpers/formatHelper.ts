4237a0abd78ec70ef580eae3a07e53a37e6c8345
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:12:12 +0200

    add (helpers) add normalizeSize, getExtension & setCSSProperty methods

---
+++ src/helpers/formatHelper.ts
@@ -0,0 +1,24 @@
+export function normalizeSize(size: number): string {
+    const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    function niceBytes(x: number): string {
+
+        let l = 0, n = parseInt(String(x), 10) || 0;
+
+        while (n >= 1024 && ++l) {
+            n = n / 1024;
+        }
+
+        return (n.toFixed(l > 0 ? 1 : 0) + ' ' + units[l]);
+    }
+
+    return niceBytes(size)
+}
+
+export function getExtension(string: string): string {
+    return string.substring(string.lastIndexOf('.') + 1);
+}
+
+export function setCSSProperty(property: string, value: string): void {
+    document.documentElement.style.setProperty(property, value);
+}
