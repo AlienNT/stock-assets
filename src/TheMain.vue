fcb76461cef3e8e4aa5e236387ce69f1748c4247
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:16:17 +0200

    add (components) create main component

---
+++ src/TheMain.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+import {setCSSProperty} from "@/helpers/formatHelper.ts";
+
+onMounted(() => {
+  setCSSProperty("--vh", `${window.innerHeight}px`)
+})
+</script>
+
+<template>
+  <main class="main">
+    <router-view v-slot="{Component}">
+      <component :is="Component"/>
+    </router-view>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  min-height: calc(var(--vh) - var(--headerH));
+}
+</style>
