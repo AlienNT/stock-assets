23d1487c58247e0f600b7e6c18484a02652a9a3b
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:16:31 +0200

    add (components) create header component

---
+++ src/TheHeader.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+import {onMounted, Ref, ref} from "vue";
+import {setCSSProperty} from "@/helpers/formatHelper.ts";
+
+const {back} = useRouter()
+function historyBack(){
+  back()
+}
+
+const header: Ref<HTMLElement> = ref(null) as unknown as Ref<HTMLElement>
+
+onMounted(() => {
+  setCSSProperty('--headerH', `${header.value.offsetHeight}px`)
+})
+</script>
+
+<template>
+<header class="header" ref="header">
+  <ul>
+    <li>
+      <router-link to="/">Images</router-link>
+    </li>
+    <li>
+      <router-link to="/videos">Videos</router-link>
+    </li>
+  </ul>
+  <button class="back" type="button" @click="historyBack">back</button>
+</header>
+</template>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background: #000;
+  padding: 15px;
+  z-index: 1;
+}
+</style>
