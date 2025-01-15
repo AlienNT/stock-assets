c5441df9f731db659611a980219ab40d08fc755d
Trysten M <nick.tristen22@gmail.com>
2025-01-15 23:18:25 +0200

    add (components) create image component

---
+++ src/components/UI/VImage.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+export interface ImageProps {
+  src: string | undefined,
+  alt: string
+  width?: string,
+  height?: string,
+  objectFit?: 'contain' | 'cover'
+  background?: string | 'transparent',
+  useTransition?: boolean,
+  transitionDuration?: number,
+  minWidth?: string,
+}
+
+const props = withDefaults(defineProps<ImageProps>(), {
+  width: '100%',
+  height: '100%',
+  background: '#ccc',
+  useTransition: true,
+  objectFit: 'contain',
+  transitionDuration: 500,
+  minWidth: '240px',
+})
+
+const style = computed(() => [
+  props.width ? `width: ${props.width}` : '',
+  props.height ? `height: ${props.height}` : '',
+  props.objectFit ? `object-fit: ${props.objectFit}` : '',
+  props.background ? `background: ${props.background}` : '',
+  props.useTransition ? `transition: ease ${props.transitionDuration / 1000}s` : '',
+  props.minWidth ? `min-width: ${props.minWidth}` : '',
+].join('; '))
+</script>
+
+<template>
+  <div class="image-wrapper">
+    <img
+        :style="style"
+        :src="props.src"
+        :alt="alt"
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+.image-wrapper {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+}
+</style>
