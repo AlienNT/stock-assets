7f36967c07d31f115f331c41153b75f803c0b4f8
Trysten M <nick.tristen22@gmail.com>
2025-01-16 05:41:13 +0200

    add (components) create SearchNotFound.vue component

---
+++ src/components/UI/SearchNotFound.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+export interface SearchNotFoundProps {
+  title?: string
+}
+
+withDefaults(defineProps<SearchNotFoundProps>(), {
+  title: 'not found'
+})
+</script>
+
+<template>
+<div class="search-not-found">
+  {{title}}
+</div>
+</template>
+
+<style scoped lang="scss">
+.search-not-found {
+  font-family: monospace;
+  text-transform: uppercase;
+  font-size: 24px;
+  text-align: center;
+}
+</style>
