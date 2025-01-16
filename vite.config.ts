0c2a2601deed658081195bbe5e18398dcc94c1fc
Trysten M <nick.tristen22@gmail.com>
2025-01-16 02:11:18 +0200

    update (config) add api key console log

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig(({mode, command}) => {
-    const {VITE_BASE_URL} = loadEnv(mode, process.cwd())
+    const {VITE_BASE_URL, VITE_API_KEY} = loadEnv(mode, process.cwd())
 
     const base = command === 'build' && !VITE_BASE_URL ?
         '/stock-assets/' :
         VITE_BASE_URL
 
-    console.log({ base, mode, command })
+    console.log({ base, VITE_API_KEY, mode, command })
 
     return {
         base,
