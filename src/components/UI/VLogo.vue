07ffb6a27ab8aee4ac11ad468f0a202fa06228a8
Trysten M <nick.tristen22@gmail.com>
2025-01-17 19:51:09 +0200

    add (components) create logo component

---
+++ src/components/UI/VLogo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import VImage from "@/components/UI/VImage.vue";
+
+export interface LogoPropsInterface {
+  src?: string;
+  link?: string;
+}
+
+withDefaults(defineProps<LogoPropsInterface>(), {
+  src: '',
+  link: '/',
+})
+</script>
+
+<template>
+  <div class="logo">
+    <router-link :to="link">
+      <VImage
+          min-width="50px"
+          min-height="50px"
+          :background="`#ccc`"
+          :src="src"
+          alt="logo"
+      />
+    </router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
