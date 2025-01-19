d0e2eb4b7157cc19941090207f584c79d02ec53a
Trysten M <nick.tristen22@gmail.com>
2025-01-19 17:10:44 +0200

    update (composable) switch axios to fetch api

--- src/composables/useApiRequest.ts
+++ src/composables/useApiRequest.ts
@@ -1,6 +1,6 @@
 import {computed, reactive} from "vue";
 import {apiConfig} from "../configs/apiConfig.ts";
-import axios, {AxiosProgressEvent, AxiosResponse} from "axios";
+import {AxiosProgressEvent} from "axios";
 
 import {useSearch} from "@/composables/useSearch.ts";
 
@@ -10,7 +10,23 @@ export interface ApiRequestInterface<RequestInterface> {
     url?: string,
 }
 
+export interface ApiResponseInterface<ResponseInterface> {
+    data: ResponseInterface;
+    status: number;
+}
+
 const {searchQuery} = useSearch()
+
+function getParamsString(params: any): string {
+    if (typeof params !== "object") {
+        return '';
+    }
+    return Object.keys(params).map((key) => `${key}=${params[key]}`).join('&')
+}
+
+function getUrlString(url: string | undefined, params: string | undefined): string {
+    return `${apiConfig.API_URL}${url ? url : ''}${params ? `?${params}` : ''}`;
+}
 
 export function useApiRequest() {
     const state = reactive({
@@ -20,38 +36,76 @@ export function useApiRequest() {
     })
 
     const isLoading = computed(() => state.isLoading);
-    const downloadProgress = computed(() => state.onDownloadProgress);
-    const uploadProgress = computed(() => state.onUploadProgress);
+    // const downloadProgress = computed(() => state.onDownloadProgress); //ибо пришлось отказаться от axios по техническим причинам (сторона API)
+    // const uploadProgress = computed(() => state.onUploadProgress);
 
-    async function request<RequestInterface, ResponseInterface>(
+    // async function request<RequestInterface, ResponseInterface>(
+    //     {
+    //         method = 'GET',
+    //         params,
+    //         url,
+    //     }: ApiRequestInterface<RequestInterface>): Promise<AxiosResponse<ResponseInterface>> {
+    //
+    //     state.isLoading = true;
+    //
+    //     return axios({
+    //         baseURL: apiConfig.API_URL,
+    //         method,
+    //         url,
+    //         params: {...params, key: apiConfig.API_KEY, q: searchQuery.value},
+    //         onDownloadProgress(progressEvent: AxiosProgressEvent) {
+    //             state.onDownloadProgress = progressEvent
+    //         },
+    //         onUploadProgress(progressEvent: AxiosProgressEvent) {
+    //             state.onUploadProgress = progressEvent
+    //         },
+    //     }).finally(() => {
+    //         state.isLoading = false
+    //     })
+    // }
+
+    async function request1<RequestInterface, ResponseInterface>(
         {
             method = 'GET',
             params,
             url,
-        }: ApiRequestInterface<RequestInterface>): Promise<AxiosResponse<ResponseInterface>> {
+        }: ApiRequestInterface<RequestInterface>): Promise<ApiResponseInterface<ResponseInterface>> {
 
         state.isLoading = true;
 
-        return axios({
-            baseURL: apiConfig.API_URL,
-            method,
-            url,
-            params: {...params, key: apiConfig.API_KEY, q: searchQuery.value},
-            onDownloadProgress(progressEvent: AxiosProgressEvent) {
-                state.onDownloadProgress = progressEvent
-            },
-            onUploadProgress(progressEvent: AxiosProgressEvent) {
-                state.onUploadProgress = progressEvent
-            },
-        }).finally(() => {
-            state.isLoading = false
+        return new Promise((resolve, reject) => {
+            try {
+                const urlString = getUrlString(url, getParamsString({
+                    ...params,
+                    key: apiConfig.API_KEY,
+                    q: searchQuery.value
+                }))
+
+                fetch(urlString, {
+                    method
+
+                }).then(async response => {
+                    resolve({
+                        data: await response.json(),
+                        status: response.status,
+                    })
+
+                }).finally(() => {
+                    state.isLoading = false
+
+                }).catch((error) => {
+                    reject(error)
+                })
+
+            } catch (e) {
+                console.log('error', e);
+                throw new Error(JSON.stringify(e))
+            }
         })
     }
 
     return {
-        request,
+        request: request1,
         isLoading,
-        downloadProgress,
-        uploadProgress,
     }
 }
