1bf0909b76c448d54f03330dadc49dc4c376bc48
Trysten M <nick.tristen22@gmail.com>
2025-01-19 17:07:35 +0200

    update (helpers) change images path

--- src/helpers/imagesHelper.ts
+++ src/helpers/imagesHelper.ts
@@ -1,14 +1,14 @@
 const path = import.meta.env.BASE_URL;
-console.log('path:' + path);
+
 export default {
-    BACK: path + 'public/svg/back.svg',
-    SEARCH: path + 'public/svg/search-svgrepo-com.svg',
-    PLAY: path + 'public/svg/play-svgrepo-com.svg',
-    SAVE: path + 'public/svg/diskette-svgrepo-com.svg',
-    USER: path + 'public/svg/user-svgrepo-com.svg',
-    LIKE: path + 'public/svg/like-svgrepo-com.svg',
-    EXPAND: path + 'public/svg/expand1-svgrepo-com.svg',
-    DOWNLOAD: path + 'public/svg/download-svgrepo-com.svg',
-    VIEWS: path + 'public/svg/look-svgrepo-com.svg',
-    IMAGES_PAGE_POSTER: path + 'public/img/sunset.jpg',
+    BACK: path + 'svg/back.svg',
+    SEARCH: path + 'svg/search-svgrepo-com.svg',
+    PLAY: path + 'svg/play-svgrepo-com.svg',
+    SAVE: path + 'svg/diskette-svgrepo-com.svg',
+    USER: path + 'svg/user-svgrepo-com.svg',
+    LIKE: path + 'svg/like-svgrepo-com.svg',
+    EXPAND: path + 'svg/expand1-svgrepo-com.svg',
+    DOWNLOAD: path + 'svg/download-svgrepo-com.svg',
+    VIEWS: path + 'svg/look-svgrepo-com.svg',
+    IMAGES_PAGE_POSTER: path + 'img/sunset.webp',
 }
