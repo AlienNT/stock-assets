596b1806cf0cd4a876824a54f7252be63ccbbf03
Trysten M <nick.tristen22@gmail.com>
2025-01-20 23:18:20 +0200

    style (components) update search form styles

--- src/components/search/SearchForm.vue
+++ src/components/search/SearchForm.vue
@@ -31,14 +31,48 @@ function onSubmit() {
         :disabled="isDisabled"
         @on-change="e => state.searchValue = e"
     />
+    <div class="search-button-wrapper">
+      <button class="search-button" type="submit"/>
+    </div>
   </form>
 </template>
 
 <style scoped lang="scss">
+$fieldBGColor: #d6d6d6;
+$searchButtonBG: "../../../public/svg/search-svgrepo-com.svg";
 .search-form {
   display: flex;
   flex: 1;
   height: 100%;
   color: white;
+  border-radius: 50px;
+  background: $fieldBGColor;
+  overflow: hidden;
+  padding: 5px;
+  align-items: center;
+}
+
+.search-button-wrapper {
+  display: flex;
+  border-radius: 50px;
+  overflow: hidden;
+  cursor: pointer;
+  background: #585a9a;
+  padding: 5px;
+  transition:  0.2s ease;
+}
+.search-button {
+  flex: 1;
+  cursor: pointer;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  mask-image: url($searchButtonBG);
+  mask-repeat: no-repeat;
+  mask-size: 60%;
+  mask-position: center center;
+  background-color: #ffffff;
 }
 </style>
