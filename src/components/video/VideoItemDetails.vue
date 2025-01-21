a3288e8f61606bf207e961e0e9bb9b157dd01e4d
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:41:15 +0200

    add (components) create video item details component

---
+++ src/components/video/VideoItemDetails.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+export interface VideoItemDetailsProps {
+  displayedQuality: string | number,
+  displayedDuration: string | number,
+}
+
+defineProps<VideoItemDetailsProps>()
+
+</script>
+
+<template>
+  <div class="video-details">
+    <div class="video-quality">
+      {{ displayedQuality }}
+    </div>
+    <div class="video-duration">
+      {{ displayedDuration }}
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.video-details {
+  display: flex;
+  gap: 5px;
+  color: #cccccc;
+}
+
+.video-quality {
+  color: white;
+  font-weight: bold;
+}
+</style>
