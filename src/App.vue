4292bee07bc45e10d96804ac133d80db4320672d
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:59:54 +0200

    update (components) update App.vue:
    add blur & focus window listeners;
    add mobile screen & focused screen watchers

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 <script setup lang="ts">
 import TheMain from "@/TheMain.vue";
 import TheHeader from "@/TheHeader.vue";
-import {onBeforeUnmount, onMounted} from "vue";
+import {onBeforeUnmount, onMounted, watch} from "vue";
 import {useUtils} from "@/composables/useUtils.ts";
 
-const {setIsMobileDevice, setIsMobileScreen} = useUtils()
+const {setIsMobileDevice, setIsMobileScreen, isFocusedScreen, setIsFocusedScreen, isMobileScreen} = useUtils()
 
 onMounted(() => {
   setIsMobileScreen();
   setIsMobileDevice()
 
-  addEventListener("resize", () => setIsMobileScreen());
+  addEventListener("resize", () => {
+    setIsMobileScreen()
+  });
+
+  addEventListener("blur", () => {
+    setIsFocusedScreen(false)
+  });
+
+  addEventListener("focus", () => {
+    setIsFocusedScreen(true)
+  });
 })
 
 onBeforeUnmount(() => {
-  removeEventListener("resize", () => setIsMobileScreen());
+  removeEventListener("resize", () => {
+    setIsMobileScreen()
+  });
 })
+
+watch(() => isFocusedScreen.value, (value) => {
+  document.title = !value ?
+      String.fromCharCode(9214) + ' Stock Assets' :
+      'Stock Assets';
+})
+
+watch(() => isMobileScreen.value, (value) => {
+  value ?
+      document.body.classList.add('mobile-screen') :
+      document.body.classList.remove('mobile-screen')
+})
+
 </script>
 
 <template>
