0ce20834eeb80f4610a5fe4a83eb54522e110a9b
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:41:55 +0200

    add (components) create page template

---
+++ src/templates/AssetsPageTemplate.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import VPoster from "@/components/UI/VPoster.vue";
+import {computed, onMounted, ref, watch} from "vue";
+import useObserver from "@/composables/useObserver.ts";
+
+export interface AssetsPageTemplateProps {
+  isShowPoster: boolean,
+  posterType: 'image' | 'video'
+  posterSrc: string,
+  title?: string,
+}
+
+defineProps<AssetsPageTemplateProps>()
+
+const emit = defineEmits(['onIntersected'])
+
+const {createObserver, entryByTarget, observeTarget} = useObserver()
+
+const templateContainer = ref(null as unknown as HTMLElement)
+
+onMounted(() => {
+  createObserver({rootMargin: '-50px'})
+  observeTarget(templateContainer.value)
+})
+
+const observeEntry = computed(() => {
+  return entryByTarget(templateContainer.value).value
+})
+
+const isIntersecting = computed(() => {
+  return observeEntry.value ? observeEntry.value.isIntersecting : false
+})
+
+watch(() => isIntersecting.value, (value) => {
+  emit('onIntersected', value)
+})
+
+</script>
+
+<template>
+  <section class="page assets-page">
+    <VPoster
+        v-if="isShowPoster && posterSrc"
+        :title="title"
+        :poster-type="posterType"
+        :src="posterSrc"
+    />
+    <div class="container">
+      <div class="row" id="assetsListViewport" ref="templateContainer">
+        <slot name="content"/>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.container {
+  min-height: var(--vh);
+  padding-top: calc(var(--headerH) + 15px);
+}
+</style>
