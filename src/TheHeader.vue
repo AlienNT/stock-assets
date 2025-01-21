1d24f113cbe0520ed8af86e612c5ad6bc2322f2d
Trysten M <nick.tristen22@gmail.com>
2025-01-21 21:01:21 +0200

    update (components) update Header:
    add styles;
    add scroll listener;
    set header bg transparent value

--- src/TheHeader.vue
+++ src/TheHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {onMounted, Ref, ref} from "vue";
+import {onMounted, reactive, Ref, ref} from "vue";
 import {setCSSProperty} from "@/helpers/formatHelper.ts";
 
 import PagesNavigation from "@/components/navigation/PagesNavigation.vue";
@@ -7,10 +7,26 @@ import VLogo from "@/components/UI/VLogo.vue";
 import SearchForm from "@/components/search/SearchForm.vue";
 
 const header: Ref<HTMLElement> = ref(null) as unknown as Ref<HTMLElement>
+const state = reactive({
+  bgOpacity: 0,
+})
 
 onMounted(() => {
-  setCSSProperty('--headerH', `${header.value.offsetHeight}px`)
+  setCSSProperty('--headerH', `${header.value?.offsetHeight}px`)
+  addEventListener('scroll', setHeaderOpacity)
 })
+
+function setHeaderOpacity() {
+  const opacity = calcOpacity(window.scrollY, window.innerHeight)
+  if (opacity === state.bgOpacity) return
+
+  setCSSProperty('--headerOpacity', `${opacity}`)
+}
+
+function calcOpacity(scrollHeight: number, windowHeight: number) {
+  const coefficient = +(scrollHeight * 2 / windowHeight).toFixed(2)
+  return coefficient <= 1 ? coefficient : 1
+}
 </script>
 
 <template>
@@ -31,13 +47,15 @@ onMounted(() => {
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+$opacity: var(--headerOpacity);
+
 .header {
-  position: sticky;
+  position: fixed;
   top: 0;
   width: 100%;
-  background: #000;
-  padding: 15px;
+  background: rgba(0, 0, 0, $opacity);
+  padding: 10px 15px;
   z-index: 2;
 
   .row {
