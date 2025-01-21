56cdf26bb77a5b2d1979bc185ebe9810d3125544
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:40:34 +0200

    add (components) create overlay tags list component

---
+++ src/components/UI/overlay/OverlayTagList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import OverlayTag from "@/components/UI/overlay/OverlayTag.vue";
+
+export interface OverlayTagListProps {
+  tags: string[];
+}
+
+defineProps<OverlayTagListProps>()
+
+const emit = defineEmits(['onTagClick'])
+</script>
+
+<template>
+  <div class="overlay-tags-list">
+    <OverlayTag
+        v-for="(tag, index) in tags"
+        :key="index"
+        :tag="tag"
+        @on-tag-click="e => emit('onTagClick', e)"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.overlay-tags-list {
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+</style>
