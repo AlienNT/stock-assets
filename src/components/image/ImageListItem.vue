9db37e3b013010ef4d6e7984318a962165864040
Trysten M <nick.tristen22@gmail.com>
2025-01-21 17:25:04 +0200

    update (components) remove unused image item props

--- src/components/image/ImageListItem.vue
+++ src/components/image/ImageListItem.vue
@@ -20,8 +20,6 @@ defineEmits(['onTagClick'])
         :id="id"
         :preview-u-r-l="previewURL"
         :webformat-u-r-l="webformatURL"
-        :preview-width="previewWidth"
-        :preview-height="previewHeight"
         :tags="tags"
         :views="views"
         :downloads="downloads"
@@ -36,7 +34,6 @@ defineEmits(['onTagClick'])
   display: flex;
   width: 100%;
   height: 100%;
-  background: rgba(220, 220, 220, 0.47);
   transition: .2s ease;
 }
 </style>
