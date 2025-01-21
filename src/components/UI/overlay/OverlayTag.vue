b07798990b48ee2eda21eab50ee7c3570ab3bbb1
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:40:19 +0200

    add (components) create overlay tag component

---
+++ src/components/UI/overlay/OverlayTag.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+export interface OverlayTagProps {
+  tag: string
+}
+
+defineProps<OverlayTagProps>()
+
+const emit = defineEmits(['onTagClick'])
+</script>
+
+<template>
+  <button
+      type="button"
+      class="tag"
+      :title="'search ' + tag"
+      @click.prevent.stop="emit('onTagClick', tag)"
+  >
+    {{ tag }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+.tag {
+  all: unset;
+  color: #efefef;
+  display: block;
+  font-size: 14px;
+
+  &:hover {
+    color: #ffffff;
+  }
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
+}
+</style>
