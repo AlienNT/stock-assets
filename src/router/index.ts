f73fbe2ba64b6abf299f9cb356a252c3d0cd901e
Trysten M <nick.tristen22@gmail.com>
2025-01-21 22:28:54 +0200

    update (router) add scrolling to saved position

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,17 @@ import routes from "@/router/routes.ts";
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.VITE_BASE_URL || '/'),
     routes,
+    scrollBehavior: (to, from, savedPosition) => {
+        if (import.meta.env.DEV) {
+            console.log({ to, from, savedPosition });
+        }
+        if (savedPosition) {
+            setTimeout(() => scrollTo({
+                behavior: "smooth",
+                top: savedPosition.top,
+                left: savedPosition.left
+            }), 0)
+        }
+    },
 })
-
 export default router;
