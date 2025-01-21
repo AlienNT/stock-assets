24e6c616182e4c613dc73bacefea34668dabbb51
Trysten M <nick.tristen22@gmail.com>
2025-01-21 21:03:18 +0200

    update (components) set image default background to transparent

--- src/components/UI/VImage.vue
+++ src/components/UI/VImage.vue
@@ -19,7 +19,7 @@ export interface ImageProps {
 const props = withDefaults(defineProps<ImageProps>(), {
   width: '100%',
   height: '100%',
-  background: '#ccc',
+  background: 'transparent',
   useTransition: true,
   objectFit: 'contain',
   transitionDuration: 500,
