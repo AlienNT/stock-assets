5b20981fd4aea6bc209c2227d3a728c3386013f8
Trysten M <nick.tristen22@gmail.com>
2025-01-21 20:48:19 +0200

    update (components) update video item component:
    change overlay list import
    add video item details
    add video component
    add styles

--- src/components/video/VideoItem.vue
+++ src/components/video/VideoItem.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import {computed, reactive, ref} from "vue";
 import {useUtils} from "@/composables/useUtils.ts";
-import ListItemOverlay from "@/components/UI/overlay/ListItemOverlay.vue";
+import {formattedQuality, formattedTime} from "@/helpers/formatHelper.ts";
+
 import {VideoHitInterface} from "@/types/VideoTypes.ts";
+
+import OverlayListItem from "@/components/UI/overlay/OverlayListItem.vue";
+import VideoItemDetails from "@/components/video/VideoItemDetails.vue";
+import VVideo from "@/components/UI/VVideo.vue";
 
 export interface VideoItemPropsInterface extends VideoHitInterface {
 }
@@ -16,11 +21,20 @@ const state = reactive({
   loaded: false,
   isPlaying: false,
   currentTime: '0:00' as string,
+  playPromise: null as unknown as Promise<void>,
 })
 const videoElement = ref(null as unknown as HTMLVideoElement)
 
 const video = computed(() => {
   return isMobileDevice.value ? props.videos.tiny : props.videos.tiny
+})
+
+const displayedQuality = computed(() => {
+  return formattedQuality(props.videos.large?.height || 0)
+})
+
+const displayedDuration = computed(() => {
+  return state.currentTime + '/' + formattedTime(props.duration)
 })
 
 function onLoad() {
@@ -31,7 +45,7 @@ function onHover(video: HTMLVideoElement) {
   state.isPlaying = true
 
   if (video.paused) {
-    video.play()
+    state.playPromise = video.play()
   }
 }
 
@@ -39,7 +53,9 @@ function onBlur(video: HTMLVideoElement) {
   state.isPlaying = false
 
   if (video.played) {
-    video.pause()
+    state.playPromise.then(() => {
+      video.pause()
+    })
   }
 }
 
@@ -48,46 +64,69 @@ function onTimeUpdate(e: Event) {
   state.currentTime = formattedTime(target.currentTime)
 }
 
-function formattedTime(number: number): string {
-  const minutes = Math.trunc(number / 60) || 0
-  const seconds = Math.trunc(number % 60) || 0
-
-  return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+function onHTMLElement(e: HTMLElement) {
+  videoElement.value = e as HTMLVideoElement
 }
-
 
 </script>
 
 <template>
   <div class="video-item">
-    <ListItemOverlay
+    <OverlayListItem
         :likes="likes"
         :downloads="downloads"
         :views="views"
         :tags="tags"
         class="video-overlay"
-        @on-tag-click="tag => emit('onTagClick', tag)"
+        @on-tag-click="(tag:string) => emit('onTagClick', tag)"
         @mouseenter="onHover(videoElement)"
         @mouseleave="onBlur(videoElement)"
     >
       <template #listItem>
-        <video
-            ref="videoElement"
+        <VVideo
+            :src="video?.url || ''"
             :poster="video?.thumbnail"
             :autoplay="state.isPlaying"
-            v-once
-            preload="none"
             muted
             loop
-            @timeupdate="onTimeUpdate"
-            @load="onLoad"
-        >
-          <source :src="video?.url"/>
-        </video>
+            preload="none"
+            @on-load="onLoad"
+            @on-time-update="onTimeUpdate"
+            @on-h-t-m-l-element="onHTMLElement"
+        />
+          <div class="play" :class="!state.isPlaying && 'play-show'"/>
       </template>
       <template #customRight>
-
+        <VideoItemDetails
+            :displayed-quality="displayedQuality"
+            :displayed-duration="displayedDuration"
+        />
       </template>
-    </ListItemOverlay>
+    </OverlayListItem>
   </div>
 </template>
+<style scoped lang="scss">
+.video-details {
+  align-items: flex-end;
+}
+.video-item {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+}
+.play {
+  opacity: 0;
+  transition: 0.2s ease;
+  visibility: hidden;
+  position: absolute;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.6);
+  mask: url("../../../public/svg/play-svgrepo-com.svg") no-repeat center / 50px;
+}
+.play-show {
+  opacity: 1;
+  visibility: visible;
+}
+</style>
