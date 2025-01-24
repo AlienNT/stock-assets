1d9ef17e761048ac2991beed5525811514259715
Trysten M <nick.tristen22@gmail.com>
2025-01-24 23:35:52 +0200

    add (components) create FileDetailsDownload.vue

---
+++ src/components/detailedPage/FileDetailsDownload.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+import {useDownloader} from "@/composables/useDownloader.ts";
+import LoaderDotted from "@/components/UI/LoaderDotted.vue";
+
+export interface FileDetailsDownloadProps {
+  items: any,
+  fileType: 'image' | 'video',
+}
+
+export interface FileDetailsDownloadItem {
+  item: any,
+}
+
+defineProps<FileDetailsDownloadProps>()
+const emit = defineEmits(['onClick'])
+
+const {isLoading, download} = useDownloader()
+
+const state = reactive({
+  selectedUrl: ''
+})
+
+function onClick(src: string) {
+  state.selectedUrl = src
+
+  download({src})
+}
+
+</script>
+
+<template>
+  <div
+      class="file-download"
+      :class="isLoading && 'disabled'"
+  >
+    <div
+        v-for="item in items"
+        class="file-download-option"
+    >
+      <slot
+          name="default"
+          :item="item"
+          :events="{
+            onClick,
+          }"
+      />
+    </div>
+    <transition name="fade" appear>
+      <div
+          v-if="isLoading"
+          class="file-loader"
+      >
+        <LoaderDotted/>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.file-download {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  position: relative;
+}
+
+.file-loader {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgb(234, 234, 234);
+  border-radius: 5px;
+}
+</style>
