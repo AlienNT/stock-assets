08b0e534f07d4a45e726e6fd85dae9a4517902d6
Trysten M <nick.tristen22@gmail.com>
2025-01-24 23:34:32 +0200

    add (components) create FileDetailsUserInfo.vue

---
+++ src/components/detailedPage/FileDetailsUserInfo.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import VImage from "@/components/UI/VImage.vue";
+
+export interface FileDetailsUserInfoProps {
+  imageUrl: string,
+  userName: string,
+}
+
+defineProps<FileDetailsUserInfoProps>()
+</script>
+
+<template>
+  <div class="user">
+    <div class="user__image">
+      <VImage
+          :src="imageUrl"
+          :alt="userName + ' avatar'"
+      />
+    </div>
+    <div class="user__name">
+      {{ userName }}
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user {
+  display: flex;
+  gap: 15px;
+  align-items: center;
+}
+
+.user__image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  overflow: hidden;
+}
+
+.user__name {
+  font-weight: 700;
+  color: #222222;
+}
+</style>
