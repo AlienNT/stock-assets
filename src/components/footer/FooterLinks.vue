88970d63aa68d059b0013a6b4f4e37184da6b46b
Trysten M <nick.tristen22@gmail.com>
2025-01-26 22:09:32 +0200

    add (components) create FooterLinks.vue

---
+++ src/components/footer/FooterLinks.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import FooterLinksItem, { FooterLinkItemPropsInterface } from '@/components/footer/FooterLinksItem.vue'
+
+export interface FooterLinksPropsInterface {
+  links: FooterLinkItemPropsInterface[]
+}
+
+defineProps<FooterLinksPropsInterface>()
+</script>
+
+<template>
+  <div class="col footer-links">
+	<ul class="row">
+	  <FooterLinksItem
+		  v-for="({href, title, linkTitle}) in links"
+		  :key="href"
+		  class="col footer-col"
+		  :title="title"
+		  :href="href"
+		  :linkTitle="linkTitle"
+		  target="_blank"
+	  />
+	</ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.footer-links {
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+
+  ul {
+	gap: 15px;
+  }
+
+  .footer-col {
+	display: flex;
+	align-items: center;
+  }
+}
+</style>
