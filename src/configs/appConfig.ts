d3a99d37f02be68e080f4effc6ceef3311e80dd5
Trysten M <nick.tristen22@gmail.com>
2025-01-26 22:08:37 +0200

    update (configs) add footer links

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -1,3 +1,9 @@
+type FooterLink = {
+    linkTitle: string,
+    href: string,
+    title: string,
+}
+
 export interface AppConfigInterface {
     MOBILE_SCREEN_WIDTH: number,
     IMAGE_LIST_COLUMNS_MAX: number,
@@ -8,6 +14,9 @@ export interface AppConfigInterface {
     VIDEO_LIST_COLUMNS_MIN: number,
     VIDEO_LIST_GAP: number,
     VIDEO_REQUEST_PER_PAGE: number,
+    AUTHOR_LINK: FooterLink,
+    SOURCE_LINK: FooterLink,
+    API_SOURCE_LINK: FooterLink
 }
 
 const appConfig: AppConfigInterface = {
@@ -20,6 +29,22 @@ const appConfig: AppConfigInterface = {
     VIDEO_LIST_COLUMNS_MIN: 1,
     VIDEO_LIST_GAP: 15,
     VIDEO_REQUEST_PER_PAGE: 10,
+    AUTHOR_LINK: {
+        href: 'https://webfolio-develop.onrender.com/',
+        title: 'Created by',
+        linkTitle: 'Trysten'
+    },
+    SOURCE_LINK: {
+        href: 'https://github.com/AlienNT/stock-assets',
+        title: 'Source code:',
+        linkTitle: 'GitHub'
+
+    },
+    API_SOURCE_LINK: {
+        href: 'https://pixabay.com/',
+        title: 'API:',
+        linkTitle: 'Pixabay'
+    }
 
 }
 export default appConfig
