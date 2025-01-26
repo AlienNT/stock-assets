0282b6cc00c848431ba087ad8a7ff7adb228853a
Trysten M <nick.tristen22@gmail.com>
2025-01-26 19:20:48 +0200

    add (config) add linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,106 @@
 import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
-    ...pluginVue.configs['flat/recommended']
-]
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
+  {
+    rules: {
+      indent: ['error', 'tab'],
+      'no-tabs': 0,
+      "vue/max-attributes-per-line": ["error", {
+        singleLine: {
+          max: 1
+        },
+        multiLine: {
+          max: 1
+        }
+      }],
+      'vue/html-indent': [
+        'error',
+        'tab',
+        {
+          attribute: 1,
+          baseIndent: 1,
+          closeBracket: 0,
+          alignAttributesVertically: true,
+          ignores: []
+        }
+      ],
+      "vue/component-name-in-template-casing": ["error", "PascalCase", {
+        registeredComponentsOnly: true,
+      }],
+      "vue/no-dupe-keys": ["error", {
+        groups: [],
+      }],
+      'vue/order-in-components': ['error', {
+        order: [
+          'el',
+          'name',
+          'key',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          ['provide', 'inject'],
+          'ROUTER_GUARDS',
+          'layout',
+          'middleware',
+          'validate',
+          'scrollToTop',
+          'transition',
+          'loading',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'emits',
+          'setup',
+          'asyncData',
+          'data',
+          'fetch',
+          'head',
+          'computed',
+          'watch',
+          'watchQuery',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          ['template', 'render'],
+          'renderError'
+        ]
+      }],
+      "comma-dangle": ["error", {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "always-multiline"
+      }],
+      "sort-imports": ["warn", {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": true,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": [
+          "all",
+          "multiple",
+          "single",
+          "none"
+        ],
+        "allowSeparatedGroups": false
+      }]
+    },
+  },
+
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  skipFormatting
+)
