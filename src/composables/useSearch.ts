d501f0bf1c96d3ab47ba3ee037a821c8724670c7
Trysten M <nick.tristen22@gmail.com>
2025-01-27 07:30:17 +0200

    update (composable) update useSearch.ts:
    add searchOrder & searchCategory state, computed value & setter
    add filterFields type, state, computed value & setter

--- src/composables/useSearch.ts
+++ src/composables/useSearch.ts
@@ -1,18 +1,65 @@
 import {computed, reactive} from "vue";
+import { BaseRequestInterface, CategoryType, OrderType } from '@/types/BasicTypes.ts'
+
+export type SearchFilterType = Omit<BaseRequestInterface, 'page' | 'per_page' | 'id' | 'key' | 'q' | 'callback' | 'order' | 'pretty' | 'lang'>
 
 const state = reactive({
     searchQuery: "" as string,
+    searchOrder: "popular" as OrderType,
+    searchCategory: "" as CategoryType,
+    filterFields: {
+        safesearch: false,
+        min_width: undefined,
+        min_height: undefined,
+        category: "" as CategoryType,
+        editors_choice: false,
+    } as SearchFilterType
 })
 
 export function useSearch() {
-    const searchQuery = computed(() => state.searchQuery)
+    const searchQuery = computed(() => {
+        return state.searchQuery
+    })
+
+    const searchOrder = computed(() => {
+        return state.searchOrder
+    })
+
+    const searchCategory = computed(() => {
+        return state.searchCategory
+    })
+
+    const filterFields = computed(() => {
+        return state.filterFields
+    })
 
     function setSearchQuery(query: string) {
         state.searchQuery = query
     }
 
+    function setSearchOrder(order: OrderType) {
+        state.searchOrder = order
+    }
+
+    function setSearchCategory(category: CategoryType) {
+        state.searchCategory = category
+    }
+
+    function setFilterFields(filterFields: SearchFilterType) {
+        state.filterFields = { ...filterFields }
+    }
+
     return {
         searchQuery,
         setSearchQuery,
+
+        searchOrder,
+        setSearchOrder,
+
+        searchCategory,
+        setSearchCategory,
+
+        filterFields,
+        setFilterFields,
     }
 }
