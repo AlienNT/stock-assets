4427b77afccbc4ccc9471c7cc3b09e853d84570c
Trysten M <nick.tristen22@gmail.com>
2025-01-28 02:13:23 +0200

    fix (router) change scroll action

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,10 +5,7 @@ const router = createRouter({
     history: createWebHashHistory(import.meta.env.VITE_BASE_URL || '/'),
     routes,
     scrollBehavior: (to, from, savedPosition) => {
-        if (import.meta.env.DEV) {
-            console.log({ to, from, savedPosition });
-        }
-        if (savedPosition) {
+        if (savedPosition && (to.fullPath !== from.fullPath)) {
             setTimeout(() => scrollTo({
                 behavior: "smooth",
                 top: savedPosition.top,
