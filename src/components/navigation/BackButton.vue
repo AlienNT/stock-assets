2d9b9b303c11a37f3e30681245a99cc91ea9393b
Trysten M <nick.tristen22@gmail.com>
2025-01-28 02:08:22 +0200

    update (components) update BackButton.vue:
    fix styles
    no redirect if history is empty

--- src/components/navigation/BackButton.vue
+++ src/components/navigation/BackButton.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import {useRouter} from "vue-router";
-import {computed} from "vue";
-import imagesHelper from "@/helpers/imagesHelper.ts";
+import { useRouter } from 'vue-router'
+import { computed, reactive, watch } from 'vue'
+import imagesHelper from '@/helpers/imagesHelper.ts'
 
 export interface BackButtonPropsInterface {
   title?: string,
@@ -12,32 +12,59 @@ export interface BackButtonPropsInterface {
 withDefaults(defineProps<BackButtonPropsInterface>(), {
   title: 'Back',
   showTitle: false,
-  showIcon: true,
+  showIcon: true
+})
+
+const { back, options, currentRoute } = useRouter()
+
+const state = reactive({
+  isBack: true
 })
 
 const style = computed(() => [
   imagesHelper.BACK ? `mask-image: url(${imagesHelper.BACK})` : ''
 ].join('; '))
 
+const fullPath = computed(() => {
+  return currentRoute.value.fullPath
+})
 
-const {back} = useRouter()
+function onHistoryBack(): void {
+  if (state.isBack) {
+	back()
+  }
+}
+
+function setIsBack() {
+  state.isBack = !!options.history.state.back
+}
+
+watch(() => fullPath.value, (value, oldValue) => {
+  if (value !== oldValue) {
+	setIsBack()
+  }
+}, {
+  immediate: true
+})
+
 </script>
 
 <template>
   <button
-      class="back-button"
-      type="button"
-      title="Back"
-      @click="back()"
+	  v-if="state.isBack"
+	  class="back-button"
+	  type="button"
+	  title="Back"
+	  @click="onHistoryBack"
   >
     <span
-        v-if="showIcon"
-        :style="style"
-        class="icon"
-    />
-    <span
-        v-if="showTitle"
-        class="title">
+		v-if="showIcon"
+		:style="style"
+		class="icon"
+	/>
+	<span
+		v-if="showTitle"
+		class="title">
       {{ title }}
     </span>
   </button>
@@ -60,14 +87,14 @@ $hoverColor: #fff;
   font-weight: bold;
   font-size: 16px;
   width: 100%;
-  height: 100%;
+  //height: 100%;
 
   &:hover {
-    color: $hoverColor;
+	color: $hoverColor;
 
-    .icon {
-      background-color: $hoverColor;
-    }
+	.icon {
+	  background-color: $hoverColor;
+	}
   }
 }
 
@@ -81,8 +108,9 @@ $hoverColor: #fff;
   mask-position: left center;
   mask-size: contain;
   mask-repeat: no-repeat;
-  height: 30px;
+  //height: 30px;
   width: 30px;
+  height: 100%;
   background-color: $color;
 }
 
