0b5e1372e3b813c8ea88b4e434bf3a275f1756fb
Trysten M <nick.tristen22@gmail.com>
2025-01-29 16:26:28 +0200

    add (components) create SearchFilterFieldText.vue

---
+++ src/components/search/fields/SearchFilterFieldText.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+ title?: string,
+ value?: string
+}>()
+
+const emit = defineEmits(['onInput'])
+</script>
+
+<template>
+  <label class="search-filter-field search-filter-field--text">
+	<input
+		class="search-filter-field__input"
+		type="text"
+		:value="value"
+		:placeholder="title"
+		@input="e => emit('onInput', e)"
+	>
+  </label>
+</template>
