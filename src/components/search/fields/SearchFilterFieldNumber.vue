8f9cd432272b32ae514d17d95fed731ecc35dbd1
Trysten M <nick.tristen22@gmail.com>
2025-01-29 16:26:41 +0200

    add (components) create SearchFilterFieldNumber.vue

---
+++ src/components/search/fields/SearchFilterFieldNumber.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+withDefaults(defineProps<{
+  title?: string,
+  value?: number,
+  min?: number,
+  max?: number,
+}>(), {
+  min: 0
+})
+
+const emit = defineEmits(['onInput'])
+</script>
+
+<template>
+  <label class="search-filter-field search-filter-filed--number">
+	<input
+		class="search-filter-field__input"
+		type="number"
+		pattern="[0-9]*"
+		:min="min"
+		:max="max"
+		:value="value"
+		:placeholder="title"
+		@input="e => emit('onInput', e)"
+	/>
+  </label>
+</template>
+
+<style scoped lang="scss">
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
