a93a76a55db903fdf3f99976d816870e6374fd8a
Trysten M <nick.tristen22@gmail.com>
2025-02-04 05:36:53 +0200

    update (components) add isFocused variable in SearchField.vue; add styles

--- src/components/search/SearchField.vue
+++ src/components/search/SearchField.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 export interface SearchFieldProps {
   value: string,
   placeholder?: string,
@@ -14,11 +16,23 @@ withDefaults(defineProps<SearchFieldProps>(), {
   disabled: false,
 })
 
-const emit = defineEmits(['onChange'])
+const emit = defineEmits(['onChange', 'onFocus', 'onBlur'])
+
+const isFocused = ref(false)
 
 function onChange(e: Event) {
   const {value} = e.target as HTMLInputElement;
   emit('onChange', value)
+}
+
+function onFocus(e: Event) {
+  isFocused.value = true
+  emit('onFocus', e)
+}
+
+function onBlur(e: Event) {
+  isFocused.value = false
+  emit('onBlur', e)
 }
 
 </script>
@@ -35,8 +49,11 @@ function onChange(e: Event) {
           :placeholder="placeholder"
           :value="value"
           :disabled="disabled"
+		  :class="isFocused && 'search-field--focused'"
           class="search-field"
           @change="onChange"
+		  @focus="onFocus"
+		  @blur="onBlur"
       >
     </label>
   </div>
@@ -45,33 +62,37 @@ function onChange(e: Event) {
 <style scoped lang="scss">
 @use 'sass:color';
 
-.search, .search-label, .search-field  {
+.search,
+.search-label,
+.search-field  {
   height: 100%;
   flex: 1;
   display: flex;
 }
-.search {
-  input {
-    font-size: 16px;
-    width: 100%;
-    padding: 8px 20px;
-    transition: background 0.2s ease, color 0.2s ease;
-    background: transparent;
+.search-field {
+  font-size: 16px;
+  width: 100%;
+  padding: 8px 20px;
+  transition: background 0.2s ease, color 0.2s ease;
+  background: transparent;
+  color: #b8b8b8;
 
-    &[disabled] {
-      user-select: none;
-      pointer-events: none;
-    }
-
-    &::placeholder {
-      transition: color 0.2s ease;
-    }
-
-    &:focus {
-      &::placeholder {
-        color: transparent;
-      }
-    }
+  &[disabled] {
+	user-select: none;
+	pointer-events: none;
   }
+
+  &::placeholder {
+	transition: color 0.2s ease;
+  }
+
+  &:focus {
+	&::placeholder {
+	  color: transparent;
+	}
+  }
+}
+.search-field--focused {
+  color: white;
 }
 </style>
